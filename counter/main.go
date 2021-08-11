8050048187d0d68d952b45367476eece86a7463a
Stelian <stoianstelian33@gmail.com>
2021-08-11 23:34:05 +0300

    Add database update for counter

--- counter/main.go
+++ counter/main.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"sync"
+	"syscall"
+	"time"
 
 	"github.com/nsqio/go-nsq"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var fatalErr error
@@ -18,6 +22,9 @@ func fatal(e error) {
 	flag.PrintDefaults()
 	fatalErr = e
 }
+
+// Time between database update attempts.
+const updateDuration = 1 * time.Second
 
 func main() {
 	defer func() {
@@ -60,5 +67,45 @@ func main() {
 	if err := q.ConnectToNSQLookupd("localhost:4161"); err != nil {
 		fatal(err)
 		return
+	}
+
+	ticker := time.NewTicker(updateDuration)
+	termChan := make(chan os.Signal, 1)
+	signal.Notify(termChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	for {
+		select {
+		case <-ticker.C:
+			doCount(&countsLock, &counts, pollData)
+		case <-termChan:
+			ticker.Stop()
+			q.Stop()
+		case <-q.StopChan:
+			return
+		}
+	}
+}
+
+func doCount(countsLock *sync.Mutex, counts *map[string]int, pollData *mgo.Collection) {
+	countsLock.Lock()
+	defer countsLock.Unlock()
+	if len(*counts) == 0 {
+		log.Println("No new votes, skipping database update")
+		return
+	}
+	log.Println("Updating database...")
+	log.Println(*counts)
+	ok := true
+	for option, count := range *counts {
+		sel := bson.M{"options": bson.M{"$in": []string{option}}}
+		up := bson.M{"$inc": bson.M{"results." +
+			option: count}}
+		if _, err := pollData.UpdateAll(sel, up); err != nil {
+			log.Println("failed to update:", err)
+			ok = false
+		}
+	}
+	if ok {
+		log.Println("Finished updating database.")
+		*counts = nil // Reset count
 	}
 }
