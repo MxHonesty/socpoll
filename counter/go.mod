873eeafded3b0915eef4feb9f17638d8d1794d30
Stelian <stoianstelian33@gmail.com>
2021-08-11 21:15:32 +0300

    Add counter nsq message consumer

---
+++ counter/go.mod
@@ -0,0 +1,8 @@
+module counter
+
+go 1.16
+
+require (
+	github.com/nsqio/go-nsq v1.0.8 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+)
---
+++ counter/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/nsqio/go-nsq"
+	"gopkg.in/mgo.v2"
+)
+
+var fatalErr error
+
+func fatal(e error) {
+	fmt.Println(e)
+	flag.PrintDefaults()
+	fatalErr = e
+}
+
+func main() {
+	defer func() {
+		if fatalErr != nil {
+			os.Exit(1)
+		}
+	}()
+
+	log.Println("Connecting to database...")
+	db, err := mgo.Dial("localhost")
+	if err != nil {
+		fatal(err)
+		return
+	}
+	defer func() {
+		log.Println("Closing database connection...")
+		db.Close()
+	}()
+	pollData := db.DB("ballots").C("polls")
+
+	var counts map[string]int
+	var countsLock sync.Mutex
+	log.Println("Connecting to nsq...")
+	q, err := nsq.NewConsumer("votes", "counter", nsq.NewConfig())
+	if err != nil {
+		fatal(err)
+		return
+	}
+	q.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+		if counts == nil {
+			counts = make(map[string]int)
+		}
+		vote := string(m.Body)
+		counts[vote]++
+		return nil
+	}))
+
+	if err := q.ConnectToNSQLookupd("localhost:4161"); err != nil {
+		fatal(err)
+		return
+	}
+}
